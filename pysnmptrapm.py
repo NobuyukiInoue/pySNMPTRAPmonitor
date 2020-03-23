e4fceabb2b68acd56c65814deb545cced42fafc9
Nobuyuki Inoue <gx3n-inue@asahi-net.or.jp>
2020-03-23 14:03:28 +0900

    support snmp version 3

--- pysnmptrapm.py
+++ pysnmptrapm.py
@@ -70,31 +70,23 @@ def select_msg_for_test():
     # command write(version 2c)
     testdata.append(b'0\x81\x88\x02\x01\x01\x04\x06public\xa7{\x02\x02\x07\xf0\x02\x01\x00\x02\x01\x000o0\x10\x06\x08+\x06\x01\x02\x01\x01\x03\x00C\x04\x01\xa3_\xa60\x19\x06\n+\x06\x01\x06\x03\x01\x01\x04\x01\x00\x06\x0b+\x06\x01\x04\x01\t\t+\x02\x00\x010\x14\x06\x0f+\x06\x01\x04\x01\t\t+\x01\x01\x06\x01\x03\x81\x13\x02\x01\x010\x14\x06\x0f+\x06\x01\x04\x01\t\t+\x01\x01\x06\x01\x04\x81\x13\x02\x01\x030\x14\x06\x0f+\x06\x01\x04\x01\t\t+\x01\x01\x06\x01\x05\x81\x13\x02\x01\x04')
 
+    # port linkdown(version 3)
+    testdata.append(b"0\x81\xe5\x02\x01\x030\r\x02\x01\x0f\x02\x02\x05\xdc\x04\x01\x00\x02\x01\x03\x04&0$\x04\x0c\x80\x00\x00\t\x03\x00\\\xfcf_6\x01\x02\x02\x00\xd6\x02\x02\x11\x1e\x04\x08username\x04\x00\x04\x000\x81\xa8\x04\x0c\x80\x00\x00\t\x03\x00\\\xfcf_6\x01\x04\x00\xa7\x81\x95\x02\x01j\x02\x01\x00\x02\x01\x000\x81\x890\x0f\x06\x08+\x06\x01\x02\x01\x01\x03\x00C\x03\x06\xcd<0\x17\x06\n+\x06\x01\x06\x03\x01\x01\x04\x01\x00\x06\t+\x06\x01\x06\x03\x01\x01\x05\x030\x11\x06\x0b+\x06\x01\x02\x01\x02\x02\x01\x01\xce}\x02\x02'}0!\x06\x0b+\x06\x01\x02\x01\x02\x02\x01\x02\xce}\x04\x12GigabitEthernet0/90\x10\x06\x0b+\x06\x01\x02\x01\x02\x02\x01\x03\xce}\x02\x01\x060\x15\x06\r+\x06\x01\x04\x01\t\x02\x02\x01\x01\x14\xce}\x04\x04down")
+
     while True:
         print("0 ... {0}\n{1}".format("command write(version 1)", testdata[0]))
         print("1 ... {0}\n{1}".format("link-down(version 1)", testdata[1]))
         print("2 ... {0}\n{1}".format("link-up(version 1)", testdata[2]))
         print("3 ... {0}\n{1}".format("command write(version 2c)", testdata[3]))
-        print("select [0-3] : ", end = "")
+        print("4 ... {0}\n{1}".format("port linkdown(version 3)", testdata[4]))
+
+        print("select [0-4] : ", end = "")
         res = input()
         try:
-            if 0 <= int(res) < 4:
+            if 0 <= int(res) <= 4:
                 return testdata[int(res)]
         except:
             continue
-
-
-def get_flds(offset, msg):
-    offset_start = offset
-    offset, current_type = get_datatype(offset, msg[offset])
-    offset, current_length = get_length(offset, msg[offset:])
-    current_data = msg[offset : offset+current_length]
-    int_current_data = int.from_bytes(current_data, "big")
-    offset += current_length
-    len_flds = offset - offset_start
-    hexdata = int.from_bytes(msg[offset_start : offset_start+len_flds], "big")
-
-    return offset_start, offset, current_type, current_length, current_data, int_current_data, len_flds, hexdata
 
 
 def print_trap_msg(msg):
@@ -136,22 +128,18 @@ def print_trap_msg(msg):
         print("SNMP Version: unknown")
         return
 
-    """ community """
-    offset_start, offset, current_type, current_length, current_data, int_current_data, len_flds, hexdata = get_flds(offset, msg)
+    if 0 <= version <= 1:
+        """ community """
+        offset_start, offset, current_type, current_length, current_data, int_current_data, len_flds, hexdata = get_flds(offset, msg)
 
-    format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x}\n{2:20s}  {3:18s} {4:s}(0x{5:0" + str(2*current_length) + "x})"
-    print(format_str.format(offset_start, hexdata, "", "community:", str(current_data, encoding="ascii"), int_current_data))
+        format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x}\n{2:20s}  {3:18s} {4:s}(0x{5:0" + str(2*current_length) + "x})"
+        print(format_str.format(offset_start, hexdata, "", "community:", str(current_data, encoding="ascii"), int_current_data))
 
-    """ data """
-    offset_start = offset
-    data_head = msg[offset]
-    offset += 1
-    offset, current_length = get_length(offset, msg[offset:])
-    len_flds = offset - offset_start
-    hexdata = int.from_bytes(msg[offset_start : offset_start+len_flds], "big")
+        """ data """
+        offset_start, offset, current_length, len_flds, hexdata = get_data(offset, msg)
 
-    format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x} {2:" + str(14 - 2*len_flds) + "s} {3:<18s}\n{4:20s}  {5:18s} {6:d}(0x{7:x})"
-    print(format_str.format(offset_start, hexdata, "", "data", "", "size:", current_length, current_length))
+        format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x} {2:" + str(14 - 2*len_flds) + "s} {3:<18s}\n{4:20s}  {5:18s} {6:d}(0x{7:x})"
+        print(format_str.format(offset_start, hexdata, "", "data", "", "size:", current_length, current_length))
 
     if version == 0:
         # version 1
@@ -185,27 +173,169 @@ def print_trap_msg(msg):
         format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x} {2:" + str(14 - 2*len_flds) + "s} {3:18s} {4:d}(0x{4:0" + str(2*current_length) + "x})"
         print(format_str.format(offset_start, hexdata, "", "time-stamp:", int_current_data))
 
+        # variable-bindings
+        offset = print_variable_bindings(offset, msg)
+
     elif version == 1:
-        # version 2c
-        """ request-id """
+        # snmp version 2c
+        offset = snmpv2_trap(offset, msg)
+
+        # variable-bindings
+        offset = print_variable_bindings(offset, msg)
+
+    elif version == 3:
+        # version 3
+        """ msgGlobalData """
+        offset_start, offset, current_length, len_flds, hexdata = get_data(offset, msg)
+
+        format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x} {2:" + str(14 - 2*len_flds) + "s} {3:<18s}\n{4:20s}  {5:18s} {6:d}(0x{7:x})"
+        print(format_str.format(offset_start, hexdata, "", "msgGlobalData", "", "size:", current_length, current_length))
+
+        """ msgID """
         offset_start, offset, current_type, current_length, current_data, int_current_data, len_flds, hexdata = get_flds(offset, msg)
 
-        format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x} {2:" + str(14 - 2*len_flds) + "s} {3:18s} {4:d}(0x{4:x})"
-        print(format_str.format(offset_start, hexdata, "", "request-id:", int_current_data))
+        format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x}\n{2:20s}  {3:18s} {4:d}(0x{4:0" + str(2*current_length) + "x})"
+        print(format_str.format(offset_start, hexdata, "", "msgID:", int_current_data))
 
-        """ error-status """
+        """ msgMaxSize """
         offset_start, offset, current_type, current_length, current_data, int_current_data, len_flds, hexdata = get_flds(offset, msg)
 
-        format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x} {2:" + str(14 - 2*len_flds) + "s} {3:18s} {4:d}(0x{4:x})"
-        print(format_str.format(offset_start, hexdata, "", "error-status:", int_current_data))
+        format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x}\n{2:20s}  {3:18s} {4:d}(0x{4:0" + str(2*current_length) + "x})"
+        print(format_str.format(offset_start, hexdata, "", "msgMaxSize:", int_current_data))
 
-        """ error-index """
+        """ msgFlags """
         offset_start, offset, current_type, current_length, current_data, int_current_data, len_flds, hexdata = get_flds(offset, msg)
 
-        format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x} {2:" + str(14 - 2*len_flds) + "s} {3:18s} {4:d}(0x{4:x})"
-        print(format_str.format(offset_start, hexdata, "", "error-index:", int_current_data))
+        format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x}\n{2:20s}  {3:18s} {4:d}(0x{4:0" + str(2*current_length) + "x})"
+        print(format_str.format(offset_start, hexdata, "", "msgFlags:", int_current_data))
+
+        """ msgSecurityModel """
+        offset_start, offset, current_type, current_length, current_data, int_current_data, len_flds, hexdata = get_flds(offset, msg)
+
+        format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x}\n{2:20s}  {3:18s} {4:d}(0x{4:0" + str(2*current_length) + "x})"
+        print(format_str.format(offset_start, hexdata, "", "msgSecurityModel:", int_current_data))
+
+        """ msgGlobalData """
+        offset_start, offset, current_length, len_flds, hexdata = get_data(offset, msg)
+
+        format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x} {2:" + str(14 - 2*len_flds) + "s} {3:<18s}\n{4:20s}  {5:18s} {6:d}(0x{7:x})"
+        print(format_str.format(offset_start, hexdata, "", "msglGlobalData", "", "size:", current_length, current_length))
+        offset += 2
+
+        """ msgAuthoritativeEngineID """
+        offset_start, offset, current_type, current_length, current_data, int_current_data, len_flds, hexdata = get_flds(offset, msg)
+
+        format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x}\n{2:20s}  {3:18s} {4:d}(0x{4:0" + str(2*current_length) + "x})"
+        print(format_str.format(offset_start, hexdata, "", "msgAuthoritativeEngineID:", int_current_data))
+
+        """ msgAuthoritativeEngineBoots """
+        offset_start, offset, current_type, current_length, current_data, int_current_data, len_flds, hexdata = get_flds(offset, msg)
+
+        format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x}\n{2:20s}  {3:18s} {4:d}(0x{4:0" + str(2*current_length) + "x})"
+        print(format_str.format(offset_start, hexdata, "", "msgAuthoritativeEngineBoots:", int_current_data))
+
+        """ msgAuthoritativeEngineTime """
+        offset_start, offset, current_type, current_length, current_data, int_current_data, len_flds, hexdata = get_flds(offset, msg)
+
+        format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x}\n{2:20s}  {3:18s} {4:d}(0x{4:0" + str(2*current_length) + "x})"
+        print(format_str.format(offset_start, hexdata, "", "msgAuthoritativeEngineTime:", int_current_data))
+
+        """ msgUserName """
+        offset_start, offset, current_type, current_length, current_data, int_current_data, len_flds, hexdata = get_flds(offset, msg)
+
+        format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x}\n{2:20s}  {3:18s} {4:s}(0x{5:0" + str(2*current_length) + "x})"
+        print(format_str.format(offset_start, hexdata, "", "msgUserName:", str(current_data, encoding="ascii"), int_current_data))
+
+        """ msgAuthenticationParameters """
+        offset_start, offset, current_type, current_length, current_data, int_current_data, len_flds, hexdata = get_flds(offset, msg)
+
+        format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x}\n{2:20s}  {3:18s} {4:s}(0x{5:0" + str(2*current_length) + "x})"
+        print(format_str.format(offset_start, hexdata, "", "msgAuthenticationParameters:", str(current_data, encoding="ascii"), int_current_data))
+
+        """ msgPrivacyParameters """
+        offset_start, offset, current_type, current_length, current_data, int_current_data, len_flds, hexdata = get_flds(offset, msg)
+
+        format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x}\n{2:20s}  {3:18s} {4:s}(0x{5:0" + str(2*current_length) + "x})"
+        print(format_str.format(offset_start, hexdata, "", "msgPrivacyParameters:", str(current_data, encoding="ascii"), int_current_data))
+
+        """ msgData """
+        offset_start, offset, current_length, len_flds, hexdata = get_data(offset, msg)
+
+        format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x} {2:" + str(14 - 2*len_flds) + "s} {3:<18s}\n{4:20s}  {5:18s} {6:d}(0x{7:x})"
+        print(format_str.format(offset_start, hexdata, "", "msgData", "", "size:", current_length, current_length))
+
+        """ contextEngineID """
+        offset_start, offset, current_type, current_length, current_data, int_current_data, len_flds, hexdata = get_flds(offset, msg)
+
+        format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x}\n{2:20s}  {3:18s} {4:d}(0x{4:0" + str(2*current_length) + "x})"
+        print(format_str.format(offset_start, hexdata, "", "contextEngineID:", int_current_data))
+
+        """ contextEngineName """
+        offset_start, offset, current_type, current_length, current_data, int_current_data, len_flds, hexdata = get_flds(offset, msg)
+
+        format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x}\n{2:20s}  {3:18s} {4:s}(0x{5:0" + str(2*current_length) + "x})"
+        print(format_str.format(offset_start, hexdata, "", "contextEngineName:", str(current_data, encoding="ascii"), int_current_data))
+
+        """ data """
+        offset_start, offset, current_length, len_flds, hexdata = get_data(offset, msg)
+
+        format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x} {2:" + str(14 - 2*len_flds) + "s} {3:<18s}\n{4:20s}  {5:18s} {6:d}(0x{7:x})"
+        print(format_str.format(offset_start, hexdata, "", "data", "", "size:", current_length, current_length))
+
+        offset = snmpv2_trap(offset, msg)
+
+        # variable-bindings
+        offset = print_variable_bindings(offset, msg)
+        
+    return
+
+def get_data(offset, msg):
+    offset_start = offset
+    data_head = msg[offset]
+    offset += 1
+    offset, current_length = get_length(offset, msg[offset:])
+    len_flds = offset - offset_start
+    hexdata = int.from_bytes(msg[offset_start : offset_start+len_flds], "big")
+
+    return offset_start, offset, current_length, len_flds, hexdata
 
 
+def get_flds(offset, msg):
+    offset_start = offset
+    offset, current_type = get_datatype(offset, msg[offset])
+    offset, current_length = get_length(offset, msg[offset:])
+    current_data = msg[offset : offset+current_length]
+    int_current_data = int.from_bytes(current_data, "big")
+    offset += current_length
+    len_flds = offset - offset_start
+    hexdata = int.from_bytes(msg[offset_start : offset_start+len_flds], "big")
+
+    return offset_start, offset, current_type, current_length, current_data, int_current_data, len_flds, hexdata
+
+
+def snmpv2_trap(offset, msg):
+    """ request-id """
+    offset_start, offset, current_type, current_length, current_data, int_current_data, len_flds, hexdata = get_flds(offset, msg)
+
+    format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x} {2:" + str(14 - 2*len_flds) + "s} {3:18s} {4:d}(0x{4:x})"
+    print(format_str.format(offset_start, hexdata, "", "request-id:", int_current_data))
+
+    """ error-status """
+    offset_start, offset, current_type, current_length, current_data, int_current_data, len_flds, hexdata = get_flds(offset, msg)
+
+    format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x} {2:" + str(14 - 2*len_flds) + "s} {3:18s} {4:d}(0x{4:x})"
+    print(format_str.format(offset_start, hexdata, "", "error-status:", int_current_data))
+
+    """ error-index """
+    offset_start, offset, current_type, current_length, current_data, int_current_data, len_flds, hexdata = get_flds(offset, msg)
+
+    format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x} {2:" + str(14 - 2*len_flds) + "s} {3:18s} {4:d}(0x{4:x})"
+    print(format_str.format(offset_start, hexdata, "", "error-index:", int_current_data))
+
+    return offset
+
+
+def print_variable_bindings(offset, msg):
     """ variable-bindings """
     offset_start = offset
     offset, current_type = get_datatype(offset, msg[offset])
@@ -253,7 +383,7 @@ def print_trap_msg(msg):
             format_str = "{0:04x}: {1:0" + str(2*len_flds) + "x}\n{2:21s} {3:18s} {4:d}(0x{4:0" + str(2*current_length) + "x})"
             print(format_str.format(offset_start, hexdata, "", "val:", int_current_data))
 
-    return
+    return offset
 
 
 def get_datatype(offset, tag):
@@ -308,7 +438,7 @@ def get_version_string(version):
         return "1"
     elif version == 1:
         return "2c"
-    elif version == 2:
+    elif version == 3:
         return "3"
     else:
         return "unkown"
